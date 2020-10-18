31826cada7dbedf0d6b29170260840293d988ff3
Sumanth Hegde <sumanthhegde13@gmail.com>
2020-10-18 23:13:48 +0530

    done some necessary modifications in login screen

--- src/com/phonebook/LoginController.java
+++ src/com/phonebook/LoginController.java
@@ -1,6 +1,33 @@
 package com.phonebook;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+
 public class LoginController {
 
+    private boolean isUser=true;
+    @FXML
+    private Button ChangeLogin;
+    @FXML
+    private Label LoginType;
+    public void initialize(){
 
+        ChangeLogin.setOnAction(actionEvent -> {
+
+        //System.out.println("Change Login Button Clicked");
+            if(isUser){
+                LoginType.setText("Admin Login");
+                ChangeLogin.setText("Login as User");
+                isUser=false;
+
+            }else {
+                LoginType.setText("User Login");
+                ChangeLogin.setText("Login as Admin");
+                isUser=true;
+            }
+
+    });
+}
 }
